a578a2dafa5d54ed06df5572c66e7eaad8b871ca
zb656 <145730190+zb656@users.noreply.github.com>
2024-11-04 21:07:43 -0500

    noise fill

--- 3/sketch.js
+++ 3/sketch.js
@@ -1,13 +1,14 @@
 // TODO: noise fill
 
-let gridDim = 16;
+let gridDim = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 
   for (let x = 0; x < width; x += gridDim) {
     for (let y = 0; y < height; y += gridDim) {
-      let mc = 255;
+      let mc = 255 * noise(x/300, y/100);
       fill(mc);
       rect(x, y, gridDim);
     }
